ae7e50d17dbff5f6c6f4c7a4bd3fa8834a05ef2d
alexeyosipov88 <aoosipov88@gmail.com>
2022-05-16 18:13:12 -0400

    tackle with variables in go

--- Main.go
+++ Main.go
@@ -4,5 +4,14 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello Go!")
+	var i int = 3
+	// var j string
+	// j = i
+	// var myName string = "hello"
+	var j string = string(i)
+	var k = 2.56
+	fmt.Println(i, j)
+
+	fmt.Printf("%T, %v", k, k)
+	fmt.Print("\n")
 }
